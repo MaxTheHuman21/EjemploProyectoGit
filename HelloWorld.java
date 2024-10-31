44e40a0c90988654e94716311c13a3b65ad5368a
Sergio Maximiliano <sergio.sanchez@cua.uam.mx>
2024-10-30 21:00:31 -0600

    Se actualizó el mensaje en HolaMundo.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package ProyectoEjemploGit;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("El tipico Hello World!");
+        System.out.println("Ahora mas bien es Hola Git!");
     }
 }
