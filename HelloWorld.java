a7b875f913a0535c69376e23c16bf92926fba13f
Sergio Maximiliano <sergio.sanchez@cua.uam.mx>
2024-10-30 20:53:42 -0600

    Se agrego el programa Hola Mundo en java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package ProyectoEjemploGit;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Ahora mas bien es Hello Git!");
+        System.out.println("El tipico Hello World!");
     }
 }
